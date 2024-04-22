33c9f486f1651b6904f31aa8d833023a9fa4c3dc
Shanmugasundaram <sundar3122001@gmail.com>
2024-04-22 07:18:06 +0530

    Handled Error like product not found by middleware

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import products from './data/products.js' 
+
 dotenv.config();// Assuming products is a module exporting the data
 import dotenv from 'dotenv'
-
+import productRoutes from './routes/productRoutes.js'
+import { notFound,errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 const port = process.env.PORT || 5000;
 
@@ -14,12 +15,7 @@ app.get('/', (req, res) => {
     res.send('I am very happy that this API is running...'); // Corrected the typo
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
-app.get('/api/products/:id', (req, res) => {
-    const product= products.find((p)=>p._id===req.params.id);
-    res.json(product);
-});
-
+app.use('/api/products',productRoutes);
+app.use(notFound); 
+app.use(errorHandler);
 app.listen(port, () => console.log(`Server is running on port ${port}`)); // Added space after "port"
